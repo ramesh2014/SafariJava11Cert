04e80db5af98ee95ebefcdbf8b8575b928bc7aac
simon <simon@dancingcloudservices.com>
2021-03-29 08:36:45 -0600

    Day 1 Break 2

---
+++ src/main/java/scope/Ex1.java
@@ -0,0 +1,76 @@
+package scope;
+
+import java.util.function.Predicate;
+
+class X {
+//  void doStuff() {
+//    System.out.println(x);
+//  }
+
+//  static {
+//    System.out.println(x);
+//  }
+//  static int x = 99;
+
+//  {
+//    System.out.println(x);
+//  }
+//  int x = 99;
+}
+
+public class Ex1 {
+//  static int x = 200;
+  public void doStuff() {
+    int x = 100;
+  }
+  public static void main(String[] args) {
+//    {
+//      System.out.println(x);
+//    }
+//    int x = 99;
+
+//    {
+//      int x;
+//      final boolean t = true;
+//      if (t) { // "constant expression"
+//        x = 100;
+//      }
+//      System.out.println(x);
+//    }
+//    { int x; }
+
+//    {
+//      int x = 99;
+//      System.out.println(Ex1.x);
+////      int x;
+//      {
+//        int x;
+//      }
+//    }
+
+//    boolean x = true;
+//    x = false;
+//      Predicate<String> ps = y -> x;
+
+//    {
+//      int x;
+//      Predicate<String> ps = x -> true;
+
+//      Predicate<String> ps = y -> { int x = 99;
+//        System.out.println(this);
+//        return true; };
+
+//    }
+
+//    for (int x = 0; x < 10; x++)
+//      ;
+//    for (int x = 0; x < 10; x++) {
+//      ;
+//    }
+//    for (int x = 0; x < 10; x++) {
+//      ;
+//    }
+//    int x;
+
+  }
+}
